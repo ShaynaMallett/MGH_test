cd26c18db83617c666d35c25e60471faac51c279
Shayna Mallett <shayners@TABLET-LFG00NF8.localdomain>
2022-02-23 15:04:52 -0700

    Q1 solved, 2,3 halfway

--- solution.py
+++ solution.py
@@ -7,40 +7,100 @@
 from tkinter import *
 import json
 import requests
-import os
+import argparse
 
-api = 'https://api-v3.mbta.com/docs/swagger/swagger.json'
-api_short = 'https://api-v3.mbta.com/docs/swagger/'
-key = '698781b4164542dfaf716672b22a1f02'
-headers = {'Authorization' : 'Bearer {0}'.format(key)}
+api = 'https://api-v3.mbta.com/'
+parser = argparse.ArgumentParser(description='Process cmd line args')
 
-def get_connection(use_key):
-    if use_key:
-        response = requests.get(api,headers=headers)
-    else:
-        respons = requests.get(api)
+'''Establish connection to API. Check that response is not null or error'''
+def get_connection():
+    print("########Get connection########")
+    global headers 
+    params = 'routes'
+    api_w_params = api + params
+    headers = {'Authorization' : 'Bearer {0}'.format(key)}
+    response = requests.get(api_w_params,headers=headers)
     if response.status_code == 200:
         return json.loads(response.content.decode('utf-8'))
     else:
         return None
-def q1(use_key):
-    if use_key:
-        params = 'routes/filter[0]'
-        api_w_params = api_short + params
-        api_w_params_test = 'https://api-v3.mbta.com/routes/?fields%5Broute%5D=short_name,long_name'
-        response = requests.get(api_w_params_test,headers=headers)
-        #print(response)
-        formatted_response = json.dumps(response.json(), sort_keys=True, indent=4)
-        print(formatted_response)
+
+'''Get routes from API, filter by type, output long_name attribute for each'''
+def q1():
+    print("########Question 1########")
+    #Get and format api response
+    #Allow API to filter to avoid receiving excess data
+    #Note, could request only route long_names, but we are using the rest of the data in q2, q3
+    #so we request and store all of it here
+    params = 'routes?filter[type]=0,1&include=stop'
+    api_w_params = api + params
+    response = requests.get(api_w_params,headers=headers)
+    formatted_response = json.dumps(response.json(),  indent=4)
+    indexed = json.loads(formatted_response)
+    print(formatted_response)
+    #Create dictionaries for storing data
+    #Hashing by route type is unnecessary per instructions,
+    #but requires only one, two element dictionary lookup (computationally inexpensive)
+    #and will be useful for organizing output
+    light_rail = {}
+    heavy_rail = {}
+    routes = {
+        0: light_rail,
+        1: heavy_rail
+    }
+
+    #Iterate routes received and store each as a dictionary element keyed by its long name
+    #Variables used for readability. Could be eliminated for efficiency
+    for route in indexed["data"]:
+        type = route["attributes"]["type"]
+        long_name = route["attributes"]["long_name"]
+        routes[type][long_name] = route["id"]
+
+    #Print respective lists of rails to console
+    print("Light Rails:")
+    print(*list(routes[0].keys()), sep='\n')
+    print("\nHeavy Rails:")
+    print(*list(routes[1].keys()), sep='\n')
+
+    #route_Names = *list(route[0].keys(), sep=',') + ',' + *list(route[1].keys(), sep=',')
+    global routeNames 
+    routeNames = ','.join(list(routes[0].values())) +',' + ','.join(list(routes[1].values()))
+    
+'''
+Get routes from API, sort by # of stops, output most and least with respective number of stops
+Output stops with two or more routes intersecting at stop.
+'''
+def q2():
+    print("########Question 2########")
+    #params = 'routes?filter[type]=0,1&filter[stop]=0,1,2'
+    params = 'stops?filter[location_type]=0,1'#filter[route]='+routeNames
+    api_w_params = api + params
+    print(api_w_params)
+    response = requests.get(api_w_params,headers=headers)
+    formatted_response = json.dumps(response.json(),  indent=4)
+    indexed = json.loads(formatted_response)
+    print(formatted_response)
+'''
+Take user input for any two stops from routes in q1, output route connecting two stops
+'''
+def q3():
+    print("########Question 3########")
+    #Dijkstra's shortest path BFS node exploration
 
 def main():
-    use_key = True
-    if get_connection(use_key) == None:
+    parser.add_argument('-k', '--APIkey', type=str, default='698781b4164542dfaf716672b22a1f02', help='Key to connect to the MBTA API')
+    args = parser.parse_args()
+    global key 
+    key = args.APIkey
+
+    if get_connection() == None:
         print("Error connecting the API")
     else:
         print("Successfully connected to MBTA API")
 
-    q1(use_key)
+    q1()
+    q2()
+    q3()
 
 '''Call main function at start of program'''    
 if __name__ == '__main__':
---
+++ tests.py
@@ -0,0 +1,7 @@
+##Shayna Mallett 
+##2/22/2022
+##Testing Solutions to MGH Coding Test Questions
+
+#Connect to API
+
+
